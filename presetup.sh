d3b701ae1dfb8d97d6cccd201d60246765340899
toco8 <39761181+toco8@users.noreply.github.com>
2020-04-05 22:08:24 +0900

    Update presetup.sh

--- presetup.sh
+++ presetup.sh
@@ -3,16 +3,19 @@
 # macOS関係のファイルを削除
 rm -r /boot/.fseventsd /boot/.Spotlight-V100
 
+# バックアップ用のディレクトリを作成
+mkdir ~/backup
+
 # IPアドレスの固定
-mv /etc/dhcpcd.conf /etc/dhcpcd.conf.bak
+mv /etc/dhcpcd.conf ~/backup
 cp ./files/dhcpcd.conf /etc/dhcpcd.conf
 
 # syslogの外部受け入れを許可
-mv /etc/rsyslog.conf /etc/rsyslog.conf.bak
+mv /etc/rsyslog.conf ~/backup
 cp ./files/rsyslog.conf /etc/rsyslog.conf
 
 # logrotateの無効化
-mv /etc/logrotate.conf /etc/logrotate.conf.bak
+mv /etc/logrotate.conf ~/backup
 cp ./files/logrotate.conf /etc/logrotate.conf
 
 # swapの無効化
