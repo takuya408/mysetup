0d11516108c0c5ed463d3d49749eb3ed27216443
toco8 <39761181+toco8@users.noreply.github.com>
2020-04-05 17:55:29 +0900

    Update presetup.sh

--- presetup.sh
+++ presetup.sh
@@ -21,13 +21,9 @@ cp /（指定）/logrotate.conf /etc/logrotate.conf
 # swapの無効化
 apt -y purge dphys-swapfile
 
-# sury.orgの登録
-wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
-
 # aptのワンライナー（更新自動化、LAMPの下準備）
-#apt update && apt -y --no-install-recommends install unattended-upgrades nginx mariadb-server php7.4-fpm php7.4-curl php7.4-json php7.4-mbstring php7.4-mysql php-libsodium php-imagick php7.4-xml php7.4-zip ghostscript
-apt update && apt -y --no-install-recommends install unattended-upgrades nginx mariadb-server php-fpm php-curl php-json php-mbstring php-mysql php-libsodium php-imagick php-xml php-zip ghostscript
+apt update && apt -y --no-install-recommends install unattended-upgrades nginx mariadb-server php-fpm php-curl php-mbstring php-mysql php-libsodium php-imagick php-xml php-zip ghostscript
+#apt --install-suggests install ghostscript
 
 # シャットダウン
 shutdown -r now
