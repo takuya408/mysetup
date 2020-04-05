283dae2be47a3adf44ef9cb35794bf87c464f794
toco8 <39761181+toco8@users.noreply.github.com>
2020-04-05 21:59:56 +0900

    Update presetup.sh

--- presetup.sh
+++ presetup.sh
@@ -18,6 +18,10 @@ cp ./files/logrotate.conf /etc/logrotate.conf
 # swapの無効化
 apt -y purge dphys-swapfile
 
+# nginxのリポジトリを登録（参照: https://nginx.org/en/linux_packages.html）
+echo "deb https://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
+curl -fsSL https://nginx.org/keys/nginx_signing.key | apt-key add -
+
 # 必要なパッケージのインストール（LAMP環境、更新自動化）
 apt update
 apt -y --no-install-recommends install nginx mariadb-server php-fpm php-curl php-mbstring php-mysql php-imagick php-xml php-zip ghostscript unattended-upgrades
