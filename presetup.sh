2e9f97e1962ec34c8a4d176b374a21baad6d0cee
toco8 <39761181+toco8@users.noreply.github.com>
2020-04-05 19:27:09 +0900

    Update presetup.sh

--- presetup.sh
+++ presetup.sh
@@ -8,15 +8,15 @@ rm -r /boot/.Spotlight-V100
 
 # IPアドレスの固定
 mv /etc/dhcpcd.conf /etc/dhcpcd.conf.bak
-cp ../files/dhcpcd.conf /etc/dhcpcd.conf
+cp ./files/dhcpcd.conf /etc/dhcpcd.conf
 
 # syslogの外部受け入れを許可
 mv /etc/rsyslog.conf /etc/rsyslog.conf.bak
-cp ../files/rsyslog.conf /etc/rsyslog.conf
+cp ./files/rsyslog.conf /etc/rsyslog.conf
 
 # logrotateの無効化
 mv /etc/logrotate.conf /etc/logrotate.conf.bak
-cp ../files/logrotate.conf /etc/logrotate.conf
+cp ./files/logrotate.conf /etc/logrotate.conf
 
 # swapの無効化
 apt -y purge dphys-swapfile
