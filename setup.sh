da6ee1186793af74e9b4dbe1e77bc440371795c1
toco8 <39761181+toco8@users.noreply.github.com>
2020-04-05 18:23:26 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ wget --no-use-server-timestamps https://ja.wordpress.org/latest-ja.tar.gz
 tar xvzf latest-ja.tar.gz -C /var/www
 
 # 所有権の変更
-chown -cR apache:apache /var/www/wordpress
+chown -cR nginx:nginx /var/www/wordpress
 find /var/www/wordpress -type f -print | xargs chmod 604
 find /var/www/wordpress -type d -print | xargs chmod 705
 chmod 400 /var/www/wordpress/wp-config.php
