e09dec4309fec0e343e2192c620d85dcb8172d90
toco8 <39761181+toco8@users.noreply.github.com>
2020-04-05 21:03:30 +0900

    Update presetup.sh

--- presetup.sh
+++ presetup.sh
@@ -19,10 +19,7 @@ cp ./files/logrotate.conf /etc/logrotate.conf
 apt -y purge dphys-swapfile
 
 # aptのワンライナー（更新自動化、LAMPの下準備）
-apt update && apt -y --no-install-recommends install unattended-upgrades nginx mariadb-server php-fpm php-curl php-mbstring php-mysql php-imagick ghostscript
-#apt -y --install-suggests install php-imagick
-#apt -y --install-suggests install ghostscript
-#apt update && apt -y --no-install-recommends install unattended-upgrades nginx mariadb-server php-fpm php-curl php-mbstring php-mysql php-libsodium php-intl php-zip
+apt update && apt -y --no-install-recommends install unattended-upgrades nginx mariadb-server php-fpm php-curl php-mbstring php-mysql php-imagick php-xml php-zip ghostscript
 
 # シャットダウン
 shutdown -r now
