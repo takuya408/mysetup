1d22ee21f19f0ae383a22e20c25eb98f696df5bc
toco8 <39761181+toco8@users.noreply.github.com>
2020-04-06 20:06:49 +0900

    Update presetup.sh

--- presetup.sh
+++ presetup.sh
@@ -4,22 +4,22 @@
 rm -r /boot/.fseventsd /boot/.Spotlight-V100
 
 # バックアップ用のディレクトリを作成
-mkdir ~/backup
+mkdir /home/pi/backup
 
 # IPアドレスの固定
-mv /etc/dhcpcd.conf ~/backup
+mv /etc/dhcpcd.conf /home/pi/backup
 cp ./files/dhcpcd.conf /etc/dhcpcd.conf
 
 # localhostの変更
-mv /etc/hosts ~/backup
+mv /etc/hosts /home/pi/backup
 cp ./files/hosts /etc/hosts
 
 # syslogの外部受け入れを許可
-mv /etc/rsyslog.conf ~/backup
+mv /etc/rsyslog.conf /home/pi/backup
 cp ./files/rsyslog.conf /etc/rsyslog.conf
 
 # logrotateの無効化
-mv /etc/logrotate.conf ~/backup
+mv /etc/logrotate.conf /home/pi/backup
 cp ./files/logrotate.conf /etc/logrotate.conf
 
 # swapの無効化
@@ -28,6 +28,10 @@ apt -y purge dphys-swapfile
 # 必要なパッケージのインストール（LAMP環境、更新自動化）
 apt update
 apt -y --no-install-recommends install nginx-light libnginx-mod-http-geoip mariadb-server php-fpm php-curl php-mbstring php-mysql php-imagick php-xml php-zip ghostscript unattended-upgrades
+
+# unattended-upgradesの設定
+mv /etc/apt/apt.conf.d/50unattended-upgrades /home/pi/backup
+cp ./files/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
 
 # nginxの設定
 cp ./files/kawagoe /etc/nginx/sites-available
