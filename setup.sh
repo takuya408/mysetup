e95e3f026008e981d0aaf1361c22321cc007876c
toco8 <39761181+toco8@users.noreply.github.com>
2020-04-06 20:04:07 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ wget -P /home/pi --no-use-server-timestamps https://ja.wordpress.org/latest-ja.t
 tar xvzf /home/pi/latest-ja.tar.gz -C /var/www
 
 # 所有権の変更
+chown -cR pi:pi /home/pi
 chown -cR www-data:www-data /var/www/wordpress
 find /var/www/wordpress -type f -print | xargs chmod 604
 find /var/www/wordpress -type d -print | xargs chmod 705
