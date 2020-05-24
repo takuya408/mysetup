a3c74fc337186d90b84c4106161973db87e004fc
toco8 <39761181+toco8@users.noreply.github.com>
2020-05-24 23:04:12 +0900

    Update and rename presetup.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -38,5 +38,16 @@ cp ./files/kawagoe /etc/nginx/sites-available
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/kawagoe /etc/nginx/sites-enabled
 
+# WordPressのダウンロード
+wget -P /home/pi --no-use-server-timestamps https://ja.wordpress.org/latest-ja.tar.gz
+tar xvzf /home/pi/latest-ja.tar.gz -C /var/www
+
+# 所有権の変更
+chown -cR pi:pi /home/pi
+chown -cR www-data:www-data /var/www/wordpress
+find /var/www/wordpress -type f -print | xargs chmod 604
+find /var/www/wordpress -type d -print | xargs chmod 705
+#chmod 400 /var/www/wordpress/wp-config.php
+
 # シャットダウン
 shutdown -r now
